eeaa03be0208f93442ae6d2afb400dbe54838e0c
Gavin Williams <gavin@fishrod.co.uk>
2012-03-23 17:37:34 +0000

    Started adding the paging JS

---
+++ application/js/app/utilities/paging.js
@@ -0,0 +1,46 @@
+var app = app || {};
+
+app.utilities = app.utilities || {};
+
+app.utilities.paging = (function(){
+	
+	document.addEventListener("click", function(event){
+
+		var target = event.target;
+				
+		while(target.nodeName !== "A" && target.getAttribute('data-method') !== "push"){
+			// We've reached the body element break!
+			if(target.parentNode.nodeName == 'HTML'){
+				target = null;
+				break;
+			}
+			target = target.parentNode;
+		}
+		
+		if(target){
+				
+		}
+		
+		event.preventDefault();
+	});
+	
+	/*
+	[].forEach.call(document.querySelectorAll('a[data-method="push"]'), function(el){
+		el.addEventListener("click", function pushCard(event){
+			var pageid = this.href.hash;
+			goToCard(pageid);
+			event.preventDefault();
+		});
+	});
+			
+			function goToCard(to){	
+				document.querySelectorAll('.card.active')[0].classList.remove('active');
+				document.querySelectorAll(to)[0].classList.add('active');
+			}
+			*/
+	
+	return {
+				
+	}
+	
+})();
