a5d9f881a76838170450c6714d87b10c6e6501a7
Gavin Williams <gavin@fishrod.co.uk>
2012-03-29 00:17:58 +0100

    Added XHR for movie search need to create models next

--- application/js/app/controller/movies.js
+++ application/js/app/controller/movies.js
@@ -4,6 +4,9 @@ app.controller = app.controller || {};
 
 app.controller.movies = function(){
 	
+	var _self = this,
+		_ajaxRequest = new XMLHttpRequest();
+	
 	this.add = function (movie){
 				
 	}
@@ -12,8 +15,45 @@ app.controller.movies = function(){
 		
 		var element = document.querySelector('#add-movie input[name="query"]');
 		element.addEventListener('keyup', function(){
-			console.log(this.value);
+			_self.search(this.value);
 		});
+		
+	}
+	
+	this.search = function(query){
+		
+		/**
+		 * Only make a search if the number of characters
+		 * are more than 3
+		 */
+		if(query.length > 3){
+			
+			/**
+			 * Cancel the current HTTP request if there is any
+			 */
+			// _ajaxRequest.abort();
+			
+			var uri = 'http://www.imdbapi.com/?t=' + query;
+			
+			_ajaxRequest.open('GET', uri, true);
+			
+			_ajaxRequest.onreadystatechange = function(){
+			
+				if(this.readyState === this.DONE){
+					
+					if(this.status == 200){
+						console.log(this.responseText);
+					} else {
+						console.log('Something went wrong');
+					}
+					
+				}
+	
+			}
+			
+			_ajaxRequest.send(null);
+			
+		}
 		
 	}
 	
